d07f5ac085708d6f9aa999ed5b6dbf4ea2004d26
fabien <fabien.winkler@outlook.fr>
2020-09-25 13:59:53 +0200

    Feat ajout container + plusieurs alert  ajout success icon

--- src/ts/FabAlert.ts
+++ src/ts/FabAlert.ts
@@ -17,6 +17,7 @@ interface FabAlertOptions {
     closeOnEscape?: boolean;
     closeOnClick?: boolean;
     position?: string;
+    limitAlert?: number;
     drag?: boolean;
     pauseOnHover?: boolean;
     progressBar?: boolean;
@@ -33,45 +34,13 @@ interface FabAlertOptions {
 
 }
 
-const defaultOptions: FabAlertOptions = {
-    id: `fab-alert-${Math.floor(new Date().getTime())}`,
-    type: '',
-    class: '',
-    title: 'Default',
-    message: 'This default alert...',
-    backgroundColor: '',
-    color: '',
-    icon: '',
-    iconText: '',
-    image: '',
-    imageWidth: 50,
-    maxWidth: null,
-    zIndex: null,
-    close: true,
-    closeOnEscape: false,
-    closeOnClick: false,
-    position: '',
-    drag: true,
-    balloon: false,
-    pauseOnHover: true,
-    progressBar: true,
-    timeoutProgress: 5000,
-    progressBarColor: '',
-    transitionIn: 'fadeInUp', // bounceInLeft, bounceInRight, bounceInUp, bounceInDown, fadeIn, fadeInDown, fadeInUp, fadeInLeft, fadeInRight, flipInX
-    transitionOut: 'fadeOut', // fadeOut, fadeOutUp, fadeOutDown, fadeOutLeft, fadeOutRight, flipOutX
-    transitionInMobile: 'fadeInUp',
-    transitionOutMobile: 'fadeOutDown',
-    onOpening: function () { },
-    onOpened: function () { },
-    onClosing: function () { },
-    onClosed: function () { },
-}
-
 const log = (msg) => console.log(msg);
 
 class FabAlert {
     /** @var options cf : FabAlertOptions interface l.1 */
     options: FabAlertOptions;
+    /** @var $elContainer Container for all alert */
+    $elContainer: HTMLElement;
     /** @var $el Alert element */
     $el: HTMLElement;
     /** @var $elIcon Icon element of alert */
@@ -90,7 +59,42 @@ class FabAlert {
      * 
      * @param {Object} options Object with custom options
      */
-    constructor(options: FabAlertOptions) {
+    constructor(options?: FabAlertOptions) {
+        const defaultOptions: FabAlertOptions = {
+            id: `fab-alert-${new Date().getTime()}`,
+            type: '',
+            class: '',
+            title: 'Default',
+            message: 'This default alert...',
+            backgroundColor: '',
+            color: '',
+            icon: '',
+            iconText: '',
+            image: '',
+            imageWidth: 50,
+            maxWidth: null,
+            zIndex: null,
+            close: true,
+            closeOnEscape: false,
+            closeOnClick: false,
+            position: '',
+            limitAlert: 3,
+            drag: true,
+            balloon: false,
+            pauseOnHover: true,
+            progressBar: true,
+            timeoutProgress: 5000,
+            progressBarColor: '',
+            transitionIn: 'fadeInUp', // bounceInLeft, bounceInRight, bounceInUp, bounceInDown, fadeIn, fadeInDown, fadeInUp, fadeInLeft, fadeInRight, flipInX
+            transitionOut: 'fadeOut', // fadeOut, fadeOutUp, fadeOutDown, fadeOutLeft, fadeOutRight, flipOutX
+            transitionInMobile: 'fadeInUp',
+            transitionOutMobile: 'fadeOutDown',
+            onOpening: function () { },
+            onOpened: function () { },
+            onClosing: function () { },
+            onClosed: function () { },
+        }
+
         if (!this._valueValid(options)) {
             this.options = defaultOptions;
         } else {
@@ -114,11 +118,31 @@ class FabAlert {
         }
         return false;
     }
+
+    /**
+     * Adapt new positon to alert if add a new alert
+     * and remove the last alert if alert.length > 6
+     */
+    _manageLimitAlert() {
+        const currentAlert: NodeListOf<HTMLElement> = this.$body.querySelectorAll('.fab-alert');
+        if (currentAlert.length > 1) {
+            for (let i = 0; i < currentAlert.length; i++) {
+                if (i !== currentAlert.length - 1) {
+                    if (currentAlert.length >= this.options.limitAlert && i === 0) {
+                        for (let j = 0; j < currentAlert.length - this.options.limitAlert; j++) {
+                            this.close(window.event, currentAlert[j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
     /** @utils end of utils function */
 
     init() {
         this.createAlert();
         this.initEvents();
+        this._manageLimitAlert();
     }
 
     /**
@@ -128,6 +152,16 @@ class FabAlert {
         if (!this._valueValid(this.options.title) && !this._valueValid(this.options.message)) {
             throw new Error(`Please, alert need title or message ...`);
         }
+
+        if (document.querySelector('.fab-alert-container') === null) {
+            this.$elContainer = document.createElement('div');
+            this.$elContainer.className = `fab-alert-container`;
+            this.$body.appendChild(this.$elContainer);
+        } else {
+            this.$elContainer = document.querySelector('.fab-alert-container');
+        }
+
+
         this.$el = document.createElement('div');
         this.$el.className = `fab-alert ${this.options.transitionIn}`;
         this.$el.id = this.options.id;
@@ -143,47 +177,47 @@ class FabAlert {
         if (this._valueValid(this.options.type)) {
             this.$el.classList.add(this.options.type);
         }
-
+        
         this.$elBody = document.createElement('div');
         this.$elBody.className = 'fab-alert-body';
         this.$el.appendChild(this.$elBody);
-
+        
         if (this._valueValid(this.options.icon)) {
             this.$elIcon = document.createElement('span');
             this.$elIcon.innerHTML = `
-                <svg id="successAnimation" class="animated" xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70">
-                    <path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
-                    <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" stroke-width="2" stroke-linecap="round" fill="transparent"/>
-                    <polyline id="successAnimationCheck" stroke="#979797" stroke-width="2" points="23 34 34 43 47 27" fill="transparent"/>
-                </svg>`;
-
+            <svg id="successAnimation" class="animated" xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70">
+            <path id="successAnimationResult" fill="#D8D8D8" d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"/>
+            <circle id="successAnimationCircle" cx="35" cy="35" r="24" stroke="#979797" stroke-width="2" stroke-linecap="round" fill="transparent"/>
+            <polyline id="successAnimationCheck" stroke="#979797" stroke-width="2" points="23 34 34 43 47 27" fill="transparent"/>
+            </svg>`;
+            
             if (this._valueValid(this.options.iconText)) {
                 this.$elIcon.title = this.options.iconText;
             }
         }
-
+        
         if (this._valueValid(this.options.title)) {
             this.$elTitle = document.createElement('strong');
             this.$elTitle.className = 'fab-alert-title';
             this.$elTitle.innerHTML = this.options.title;
             this.$elBody.appendChild(this.$elTitle);
         }
-
+        
         if (this._valueValid(this.options.message)) {
             this.$elMsg = document.createElement('p');
             this.$elMsg.className = 'fab-alert-message';
             this.$elMsg.innerHTML = this.options.message;
             this.$elBody.appendChild(this.$elMsg);
         }
-
+        
         if (this._valueValid(this.options.close)) {
             this.$elClose = document.createElement('button');
             this.$elClose.className = 'fab-alert-close';
             this.$elClose.title = 'Close';
             this.$el.appendChild(this.$elClose);
         }
-
-        this.$body.appendChild(this.$el);
+        
+        this.$elContainer.appendChild(this.$el);
     }
 
     /**
@@ -197,27 +231,29 @@ class FabAlert {
 
     }
 
-    close() {
-        if (window.event) {
-            const event = window.event;
+    close(event?: Event, elem?: HTMLElement) {
+        const that = this;
+        event = event || window.event;
+        elem = elem || this.$el;
 
+        if (event) {
             event.preventDefault();
             event.stopPropagation();
         }
+
         if (this.options.onClosing && typeof this.options.onClosing === 'function') {
             this.options.onClosing(this);
         }
 
-        this.$el.classList.remove(this.options.transitionIn);
-        this.$el.classList.add(this.options.transitionOut);
+        elem.classList.remove(this.options.transitionIn);
+        elem.classList.add(this.options.transitionOut);
 
-        setTimeout(() => {
-            this.$el.remove();
-        }, 2000);
+        elem.addEventListener('animationend', function () {
+            this.remove();
+        })
 
         if (this.options.onClosed && typeof this.options.onClosed === 'function') {
             this.options.onClosed(this);
         }
     }
-
 }
